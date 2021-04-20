97af51e0e5dabffda0fbb0961d8140ebd8cb911e
Karan Bhatt <karanb@nvidia.com>
2021-04-20 12:35:30 -0500

    remove tests

--- tests/parse_docker.py
+++
@@ -1,7 +0,0 @@
-from dockerfile_parse import DockerfileParser
-
-dfp = DockerfileParser(path = "Dockerfile")
-
-for struct in dfp.structure:
-    if struct['instruction'] == "FROM":
-        print(struct['content'].strip())
